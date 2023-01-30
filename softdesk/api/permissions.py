628bf2fe6866683d76914cf2ad7828b45a29cafc
Hunold <raphael.hunold@gmail.com>
2023-01-30 17:38:59 +0100

    flake8 and fix on a bug permission

--- softdesk/api/permissions.py
+++ softdesk/api/permissions.py
@@ -1,5 +1,4 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-from rest_framework.exceptions import APIException
+from rest_framework.permissions import BasePermission
 from django.shortcuts import get_object_or_404
 from .models import Contributor, Project
 
@@ -8,7 +7,7 @@ class IsProjectContributor(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         user_id = user.id
-        
+
         contributions_of_user = [contrib.project_id for contrib in user.contributors.filter(user=user_id)]
 
         if 'project_pk' in view.kwargs:
@@ -16,14 +15,14 @@ class IsProjectContributor(BasePermission):
         elif 'pk' in view.kwargs:
             project_id = int(view.kwargs['pk'])
         else:
-            project_id = None         
-        
+            project_id = None
+
         if contributions_of_user is None:
-            return print(f"This user is not associated to any project.")
+            return print("This user is not associated to any project.")
 
         if project_id is None:
             return True
-        
+
         if project_id in contributions_of_user:
             return True
 
@@ -39,8 +38,8 @@ class IsProjectCreator(BasePermission):
 
         project_id = view.kwargs.get('project_pk')
         project = get_object_or_404(Project, id=project_id)
-        
+
         user_contrib = Contributor.objects.get(project=project, user=request.user)
-                
+
         if user_contrib.permission == 'CREATOR' and view.action in ['create', 'destroy']:
             return True
