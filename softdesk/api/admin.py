edf7a61791ca91b80c5d31098c19fe741026b27e
Hunold <raphael.hunold@gmail.com>
2023-01-30 17:12:22 +0100

    Ajout des Issues, Commments et Contributor dans l'admin

--- softdesk/api/admin.py
+++ softdesk/api/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from .models import User, Project
+from .models import User, Contributor, Project, Issue, Comment
+
 
 class UserAdmin(admin.ModelAdmin):
     pass
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    pass
+
 
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
 
+class IssueAdmin(admin.ModelAdmin):
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Comment, CommentAdmin)
