7c06c2b8104fac0c9da3dbfd8d010ef39f44e51b
Hunold <raphael.hunold@gmail.com>
2023-01-30 17:38:35 +0100

    flake8 and fix

--- softdesk/api/permissions.py
+++ softdesk/api/permissions.py
@@ -1,54 +1,34 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.exceptions import APIException
-from .models import Contributor
-
-
-
-def get_project_id(kwargs):
-    project_id = kwargs['project_pk'] if 'project_pk' in kwargs else kwargs['pk'] if 'pk' in kwargs else None
-    if project_id is None:
-        return project_id
-    else:
-        try:
-            return int(project_id)
-        except ValueError:
-            raise APIException({"details": 'Project ids must be integers.'})
+from django.shortcuts import get_object_or_404
+from .models import Contributor, Project
 
 
 class IsProjectContributor(BasePermission):
-    # message = "You don't have access to this project"
-
     def has_permission(self, request, view):
-        # List the projects on which the user is a contributor or creator
         user = request.user
-
-        if user.is_superuser:
-            return True
-        
-        project_id = get_project_id(view.kwargs)        
         user_id = user.id
-                
+        
         contributions_of_user = [contrib.project_id for contrib in user.contributors.filter(user=user_id)]
+
+        if 'project_pk' in view.kwargs:
+            project_id = int(view.kwargs['project_pk'])
+        elif 'pk' in view.kwargs:
+            project_id = int(view.kwargs['pk'])
+        else:
+            project_id = None         
         
         if contributions_of_user is None:
-            return print("This user is not associated to any project.")
-            
-        if project_id is None or project_id in contributions_of_user:
+            return print(f"This user is not associated to any project.")
+
+        if project_id is None:
+            return True
+        
+        if project_id in contributions_of_user:
             return True
 
 
-    def has_object_permission(self, request, view, obj):
-        # List of method allowed for the user on a project
-        user = request.user
-        user_id = user.id        
-        project_id = get_project_id(view.kwargs)
-        contributions_of_user = [contrib.project_id for contrib in user.contributors.filter(user=user_id)] # , permission="CONTRIBUTOR"
-
-        if project_id in contributions_of_user: # and view.action in ['list', 'retrieve']
-            return True
-
-
-class IsObjectCreator(BasePermission):    
+class IsObjectAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         if obj.author_user == request.user or view.action in ['list', 'retrieve']:
             return True
@@ -56,7 +36,11 @@ class IsObjectCreator(BasePermission):
 
 class IsProjectCreator(BasePermission):
     def has_object_permission(self, request, view, obj):
-        project_id = get_project_id(view.kwargs)
-        user_contrib = Contributor.objects.get(project_id=project_id, user=request.user)
-        if user_contrib.permission == 'CREATOR':
+
+        project_id = view.kwargs.get('project_pk')
+        project = get_object_or_404(Project, id=project_id)
+        
+        user_contrib = Contributor.objects.get(project=project, user=request.user)
+                
+        if user_contrib.permission == 'CREATOR' and view.action in ['create', 'destroy']:
             return True
